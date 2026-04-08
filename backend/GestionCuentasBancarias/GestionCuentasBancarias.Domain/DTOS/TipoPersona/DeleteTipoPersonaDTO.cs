8cc69cd1f20031901ef671d4d705893ca5a6cd40
Jose Pablo Hernandez Raymundo <josepablohr@outlook.es>
2026-04-07 23:25:43 -0600

    realizaron cambios

---
+++ backend/GestionCuentasBancarias/GestionCuentasBancarias.Domain/DTOS/TipoPersona/DeleteTipoPersonaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCuentasBancarias.Domain.DTOS.TipoPersona
+{
+    public class DeleteTipoPersonaDTO
+    {
+        public int TIP_Tipo_Persona { get; set; }
+    }
+
+}
